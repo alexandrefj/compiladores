db9cee1d4dfd1e53186878756fedc63c618305ad
afjohn1 <alexandre.flores.john@gmail.com>
2013-08-15 23:32:22 -0300

    teste das listas atualizado

--- tests/lists_test.c
+++ tests/lists_test.c
@@ -4,22 +4,47 @@
 
 int main()
 {
-	int a = 5;
-	List* lista = list_create();
-	
-	lista = list_insert(lista, a);
-	a++;
-	lista = list_insert(lista, a);
-	a++;
-	lista = list_insert(lista, a);
+	List* list = list_init();
+	List* aux;
 
+	list = list_insert(list, 0);
+	list = list_insert(list, 1);
+	list = list_insert(list, 2);
+	list = list_insert(list, 3);
+	list = list_insert(list, 4);
+	list = list_insert(list, 5);
+	list = list_insert(list, 6);
+	list = list_insert(list, 7);
+	list = list_insert(list, 8);
+	list = list_insert(list, 9);
+	list = list_insert(list, 10);
+	list = list_insert(list, 11);
+	list_print(list);
+
+	aux = list_lookup(list,3);
+	printf("|LOOKUP TEST\n");
+	printf("value = %d, value->next = %d\n", aux->node, aux->next->node);
+
+	list = list_remove(list,1);
+	list = list_remove(list,2);
+	list = list_remove(list,3);
+	list = list_remove(list,5);
+	list = list_remove(list,6);
+	list = list_remove(list,7);
 	
-	printf("Nodo[%d]->%d\n", 0,lista->node);
-	lista = lista->next;
-	printf("Nodo[%d]->%d\n", 1,lista->node);
-	lista = lista->next;
-	printf("Nodo[%d]->%d\n", 2,lista->node);
-	
+	list_print(list);
+	printf("\n");
+	list = list_remove(list,11);
+	list = list_remove(list,4);
+	list = list_remove(list,0);
+	list = list_remove(list,9);
+	list = list_remove(list,10);
+	list = list_remove(list,11);
+	list = list_remove(list,15);
+	list = list_remove(list,8);
+
+	list_print(list);
+
 
 
 return 0;
