bcb08e01b63051d6eb7383b8c4a2a2191b716002
Thiago Martins <thiagomartins89@gmail.com>
2013-08-16 01:24:36 -0300

    Adicionei o arquivo de teste dos grafos.
    Signed-off-by: Thiago Martins <thiagomartins89@gmail.com>

---
+++ tests/graph_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "comp_graph.h"
+
+int main()
+{
+	printf("\n");
+
+	Graph* graph = NULL;
+	/*createGraph(graph, 6);
+
+	addEdge(graph, 2, 1);
+	addEdge(graph, 1, 3);
+	addEdge(graph, 1, 0);
+	addEdge(graph, 1, 4);
+	addEdge(graph, 3, 5);
+	addEdge(graph, 3, 4);
+	addEdge(graph, 0, 4);
+
+	printGraph(graph);*/
+
+	return 0;
+}
