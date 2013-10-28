fbb51b3cf95c016ed3b51692be22e2867a606de6
afjohn1 <alexandre.flores.john@gmail.com>
2013-10-28 15:03:01 -0200

    ED lista has been modified in order to calculate array address

--- include/comp_list.h
+++ include/comp_list.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 #ifndef COMP_LIST_H
@@ -9,6 +10,7 @@ typedef struct comp_list_t
 {
   int type;
   int tam;
+  int size;
   char* text;
   struct comp_list_t *next;
 } 
@@ -17,13 +19,15 @@ List;
 
 
 List* list_init(void);
+List* JoinGlobalVet(List* global,List* vet);
 void list_print(List* list);
 List* TILL_NULL(List* list);
 List* FunctionParamList(List* AllParams, int function_id);
-List* list_insert(List* list, int type,int tam, char* text);
+List* list_insert(List* list, int type,int tam, char* text,int size);
 List* list_lookup(List* list, char* text);
 List* list_copy(List* list1, List* list2);
 List* list_remove(List* list, char* text);
+List* InvertList(List* list);
 
 
 #endif
--- src/comp_list.c
+++ src/comp_list.c
@@ -1,56 +1,58 @@
-#include<stdlib.h>
-#include<stdio.h>
 #include "comp_list.h"
-#include <string.h>
 
+List* InvertList(List* list){
+	List* aux =list;
+	while(list!= NULL){
+		list = list->next;
+		aux->next = list;
+	}
+	return aux;
+}
 
 List* FunctionParamList(List* AllParams, int function_id){
-
 	int found = 0;
 	List* aux = AllParams;
 	List* resultant = list_init();
- 
 	if(aux!= NULL){
-
 		while(aux != NULL){
-
 			if(aux->tam == function_id){
 				while(aux != NULL && (aux->tam == function_id)){ 
-					resultant = list_insert(resultant,aux->type,aux->tam,aux->text);
+					resultant = list_insert(resultant,aux->type,aux->tam,aux->text,0);
 					aux = aux->next;
 				}
 				return resultant;
-			}
-			aux = aux->next;
+			}aux = aux->next;
 		}
 	}
 	return NULL;
-
-
 }
 
 List* TILL_NULL(List* list){
-
 	while(list->next != NULL)
 		list = list->next;
-
 	return list;
 }
 
-List* list_copy(List* list1, List* list2)
-{
-	list1 = (List*)calloc(1,sizeof(List*));
+List* JoinGlobalVet(List* global,List* vet){
+
+	List* aux = TILL_NULL(global);
+	list_print(aux);
+	aux->next = vet;
+	return aux;
+
+}
+
+
+List* list_copy(List* list1, List* list2){
+	list1 = (List*)calloc(1,sizeof(List));
 	list1 = NULL;
-
 	List* aux;
-
 	while(list2 != NULL){
-
-		List* new  = (List*)calloc(1,sizeof(List*));
+		List* new  = (List*)calloc(1,sizeof(List));
 		new->type = list2->type;
 		new->tam = list2->tam;
+		new->size = list2->size; 
 		new->text = list2->text;
-
 		if(list1==NULL){
 			list1 = new;
 			aux = list1;
@@ -59,137 +61,98 @@ List* list_copy(List* list1, List* list2)
 			list1->next = new;
 			list1 = new;
 		}
-		
 		list2 = list2->next;
-	}
-			
+	}		
 	return aux;
-
 }
 
-
-List* list_init(void)
-{
-	List* list = (List*)calloc(1,sizeof(List*));
+List* list_init(void){
+	List* list = (List*)calloc(1,sizeof(List));
 	list = NULL;
 	return list;
 }
 
-List* list_insert(List* list, int type,int tam, char* text)
-{
-
-	List* new = malloc(sizeof(List*));
+List* list_insert(List* list, int type,int tam, char* text, int size){
+	List* new = malloc(sizeof(List));
 	new->tam = tam;	
 	new->type = type;
 	new->text = text;
+	new->size = size;
 	new->next = NULL;
-
 	if(list == NULL)
 		list = new;
-	else 				
-	{
+	else{
 		List* aux = list;
-
 		while(aux->next != NULL)
 			aux = aux->next;
-
 		aux->next = new;
 	}	
-	
 	return list;
-
 }
 
-void list_print(List* list)
-{
+void list_print(List* list){
 	List* aux = list;
-	
-	if(list!= NULL)
-	{
-		while(aux != NULL)
-		{
+	if(list!= NULL){
+		while(aux != NULL){
 			printf("type = %d ", aux->type);
 			printf("tam= %d ", aux->tam);
-			printf("text = %s\n", aux->text);
+			printf("text = %s ", aux->text);
+			printf("size = %d\n", aux->size);
 			aux = aux->next;
 		}
 	}
 	else
 		printf("EMPTY LIST\n");
-
 }
 
 
-List* list_lookup(List* list, char* text)
-{
-
+List* list_lookup(List* list, char* text){
 	if(list == NULL)
 		return NULL;
-	else 				
-	{
+	else{
 		List* aux = list;
-
-		while(aux != NULL)
-		{	
+		while(aux != NULL){	
 			if(strcmp(aux->text, text) == 0)
 				return aux;
 			else
 				aux = aux->next;
 		}
-
 		return NULL;
-
 	}	
 }
 
-List* list_remove(List* list, char* text)
-{
+List* list_remove(List* list, char* text){
 	int found =0;
-
-	if(list != NULL)				
-	{
+	if(list != NULL){
 		List* aux = list;
 		List* aux_ant = list;
-
-		while(aux != NULL && !found)
-		{	
-			if(strcmp(aux->text, text) == 0)
-			{
+		while(aux != NULL && !found){	
+			if(strcmp(aux->text, text) == 0){
 				found = 1;
-
-				if(aux==list && aux_ant==list && aux->next!=NULL)
-				{
+				if(aux==list && aux_ant==list && aux->next!=NULL){
 					list = aux->next;
 					aux = NULL;
 				}
-				else if(aux==list && aux_ant==list && aux->next==NULL)
-				{
+				else if(aux==list && aux_ant==list && aux->next==NULL){
 					list = NULL;
 					aux = NULL;
 				}
-				else if(aux->next==NULL && aux_ant==list)
-				{
+				else if(aux->next==NULL && aux_ant==list){
 					aux_ant->next = aux->next;
 					aux = NULL;
 					list = aux_ant;
 				}				
-				else
-				{
+				else{
 					aux_ant->next = aux->next;
 					aux = NULL;
 				}	
 			}
-				
-			if(!found)
-			{
-				if(aux != list ) 		// at the first time, aux_ant wont be forwarded
+			if(!found){
+				if(aux != list ) 		
 					aux_ant = aux_ant->next;
 				aux = aux->next;
 			}
-			
 		}
-
 	}	
-
 	return list;
 }
